4ebb20f09575fe86baa40faa887c1adbaf18b5b7
amandastss <amandassantos3103@gmail.com>
2026-03-12 11:33:58 -0300

    FEAT: inclusão do relacionamento de Livro com Categoria e Editora

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -2,14 +2,8 @@ from django.db import models
 
 
 class Categoria(models.Model):
-<<<<<<< HEAD
+
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
-=======
-      descricao = models.CharField(max_length=100)
-       
-      def __str__(self):
-        return f"{self.id} - {self.descricao}"
->>>>>>> 211d0b8df34754ddad74c8c911ec662da4c2b3f4
